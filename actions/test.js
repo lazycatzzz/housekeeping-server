acc409a89803df1ac10ac3c391b6f50f8c49d2f3
lazycatzzz <873125046@qq.com>
2021-02-20 16:42:18 +0800

    feat: 尝试

--- actions/test.js
+++ actions/test.js
@@ -6,5 +6,6 @@ module.exports = async function (req, res) {
   let sqlArr = [];
   const data = await dbconfig.sySqlConnect(sql, sqlArr);
   if (!data) return res.sendResult(null, 400, "获取test失败");
+  console.log(data)
   return res.sendResult(data, 200, "获取test成功");
 };

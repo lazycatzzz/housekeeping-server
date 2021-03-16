c14b6a03123323b52ef8c94bf592f98e58ec9f84
lazycatzzz <873125046@qq.com>
2021-03-16 18:25:41 +0800

    feat: 获取候选列表

--- actions/follow/list.js
+++ actions/follow/list.js
@@ -2,9 +2,23 @@
 let dbconfig = require("../../modules/dbconfig");
 
 module.exports = async function (req, res) {
+  // 获取候选id
   let sql = "select * from follow where uid = ?";
   let sqlArr = [req.params.uid];
   const data = await dbconfig.sySqlConnect(sql, sqlArr);
-  if (!data) return res.sendResult(null, 200, "获取候选列表失败");
-  return res.sendResult(data, 200, "获取候选列表成功");
+  if (!data) return res.sendResult(null, 400, "获取候选id失败");
+  if (data.length <= 0) return res.sendResult(null, 200, "没有候选");
+  // 根据id多值查找
+  const sidArr = [];
+  for (let i = 0; i < data.length; i++) {
+    sidArr.push(data[i].sid);
+  }
+  const idStr = sidArr.join(",");
+  sql =
+    `select id,name,avatar_img,tags,is_normal,is_older,is_child,is_excellent from nanny where id in (${idStr})`;
+  sqlArr = [];
+  const list = await dbconfig.sySqlConnect(sql, sqlArr);
+  console.log(list);
+  if (!list) return res.sendResult(null, 400, "获取候选列表失败");
+  return res.sendResult(list, 200, "获取候选列表成功");
 };

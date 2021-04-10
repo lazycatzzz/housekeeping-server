1cdd529ade512841663f18e5245be5277b9ba226
lazycatzzz <873125046@qq.com>
2021-04-10 17:07:30 +0800

    feat: 增加城市字段

--- actions/list/older.js
+++ actions/list/older.js
@@ -2,7 +2,7 @@
 let dbconfig = require("../../modules/dbconfig");
 
 module.exports = async function (req, res) {
-  let sql = "select id,name,avatar_img,tags,is_normal,is_older,is_child,is_excellent from nanny where is_older = 1";
+  let sql = "select id,name,avatar_img,tags,is_normal,is_older,is_child,is_excellent,location,intention from nanny where is_older = 1 And can_work = 1";
   let sqlArr = [];
   const data = await dbconfig.sySqlConnect(sql, sqlArr);
   if (!data) return res.sendResult(null, 400, "获取保姆数据失败");

4fb296bf020a4cd2114e068b998dcfcfc8094596
lazycatzzz <873125046@qq.com>
2021-04-10 16:41:46 +0800

    feat: 订单列表

--- actions/order/list.js
+++ actions/order/list.js
@@ -5,6 +5,16 @@ module.exports = async function (req, res) {
   let sql = "select * from hkorder where uid = ?";
   let sqlArr = [req.params.uid];
   const data = await dbconfig.sySqlConnect(sql, sqlArr);
-  if (!data) return res.sendResult(null, 200, "获取订单列表失败");
-  return res.sendResult(data, 200, "获取订单列表成功");
+  if (!data) return res.sendResult(null, 200, "获取订单id列表失败");
+  if (data.length === 0) return res.sendResult(null, 200, "订单为空");
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].is_reply !== 0) continue;
+    let tmp = `select avatar_img,name from nanny where id = ?`;
+    let arr = [data[i].sid];
+    console.log('helllllo')
+    const obj = await dbconfig.sySqlConnect(tmp, arr);
+    data[i] = {...data[i], ...obj[0]}
+  }
+  console.log(data)
+  return res.sendResult(data, 200, "获取订单成功");
 };

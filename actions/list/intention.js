9e390679b2b3adbeb02fbc5b39c02a8c6d690017
lazycatzzz <873125046@qq.com>
2021-04-27 02:27:02 +0800

    feat: 增加字段

--- actions/list/intention.js
+++ actions/list/intention.js
@@ -2,7 +2,7 @@
 let dbconfig = require("../../modules/dbconfig");
 
 module.exports = async function (req, res) {
-  let sql = "select id,name,avatar_img,tags,is_normal,is_older,is_child,is_excellent,location,intention from nanny where intention = ?";
+  let sql = "select id,name,age,avatar_img,tags,is_normal,is_older,is_child,is_excellent,location,intention,older_type,salary,child_age from nanny where intention = ? And can_work = 1";
   let sqlArr = [req.params.intention];
   const data = await dbconfig.sySqlConnect(sql, sqlArr);
   if (!data) return res.sendResult(null, 400, "获取保姆数据失败");

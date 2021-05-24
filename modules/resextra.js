da76ec829b4f160ecb9191d0f3ae28388a354e2b
lazycatzzz <873125046@qq.com>
2021-05-24 22:04:31 +0800

    feat: 模拟真机延迟

--- modules/resextra.js
+++ modules/resextra.js
@@ -1,17 +1,19 @@
 // 添加统一的返回结果方法
-module.exports = function(req, res, next){
-	res.sendResult = function(data,code,message) {
-		let fmt = req.query.fmt ? req.query.fmt : "rest";
-		if(fmt == "rest") {
-			res.json(
-			{
-				"data" : data, 
-				"meta" : {
-					"msg" 		: message,
-					"status" 	: code
-				}
-			});
+module.exports = function (req, res, next) {
+  res.sendResult = function (data, code, message) {
+    let fmt = req.query.fmt ? req.query.fmt : "rest";
+    if (fmt == "rest") {
+      res.json({
+        data: data,
+        meta: {
+          msg: message,
+          status: code,
+        },
+      });
     }
   };
-	next();
-}
+  // 模拟真实线上环境
+  setTimeout(() => {
+    next();
+  }, 1000);
+};
